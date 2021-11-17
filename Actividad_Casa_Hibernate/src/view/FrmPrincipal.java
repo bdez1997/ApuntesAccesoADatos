92ff4f182f1b91c959f75d5a1c523a7f7adbed26
Antonio José <bdez_1997@live.com>
2021-11-17 14:34:40 +0100

    tratra

--- Actividad_Casa_Hibernate/src/view/FrmPrincipal.java
+++ Actividad_Casa_Hibernate/src/view/FrmPrincipal.java
@@ -1,9 +1,34 @@
 package view;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import model.Casa;
+
 public class FrmPrincipal {
 	
 	public static void main(String[] args) {
+		org.jboss.logging.Logger logger = org.jboss.logging.Logger.getLogger("org.hibernate");
+		java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
 		
+		SessionFactory conf = new Configuration().configure().buildSessionFactory();
+		
+		Session session = conf.openSession();
+		
+		Transaction tx = session.beginTransaction();
+		
+		Casa c = new Casa("Membrillo", 8, 154.25);
+		// GUARDAR OBJETO session.save(c);
+		session.save(c);
+		System.out.println("se ha añadido una casa "+  c.toString());
+		
+		tx.commit();
+		
+		session.close();
+		
+		conf.close();
 	}
 
 }
